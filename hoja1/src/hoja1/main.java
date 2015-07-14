3cfa53f90ec76f2acf281c0f4c8e5c2760098be4
Jose R <josecorona1460@gmail.com>
2015-07-14 12:28:55 -0600

    primera modificacion

---
+++ hoja1/src/hoja1/main.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+//Jose Ramon Corona Boch
+//Samuel Diaz
+//Julio Dieguea Ochoa
+//13/7/15
+//HDT1
+
+
+import javax.swing.JFrame;
+
+
+
+public final class main extends JFrame {
+
+    private final int OFFSET = 30;
+
+    public main() {
+        InitUI();
+    }
+
+    public void InitUI() {
+        gui board = new gui();
+        add(board);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 160);
+        setLocationRelativeTo(null);
+        setTitle("Radio_Sonica");
+    }
+
+    public static void main(String[] args) {
+        main v = new main();
+        v.setVisible(true);
+    }
+}
